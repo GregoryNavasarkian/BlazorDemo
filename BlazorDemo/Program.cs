b0bab7a32e33e8a42249b475d7c4f61042d699f4
grego <grego@GREGORYPC>
2024-06-22 03:24:38 -0700

    Add project files.

---
+++ BlazorDemo/Program.cs
@@ -0,0 +1,13 @@
+using BlazorDemo;
+using DevExpress.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDevExpressBlazor(configure => configure.BootstrapVersion = BootstrapVersion.v5);
+
+await builder.Build().RunAsync();
